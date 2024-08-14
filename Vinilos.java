25be913b348d7c89eef91050c3b951190288afcd
Camilo-ICI24 <c.cifuentes14@ufromail.cl>
2024-08-14 10:07:10 -0400

    Add files via upload

---
+++ Vinilos.java
@@ -0,0 +1,23 @@
+package Vinilos;
+
+import java.util.Scanner;
+
+class coleccionDeVinilos {
+    public static void main(String[] args) {
+        String informacion [][] = new String [100] [3];
+        agregarDisco(informacion)
+
+    }
+
+    public static String [][] agregarDisco(String m[][], String grupo, String nombre, String salida) {
+        Scanner info = new Scanner(System.in);
+        System.out.print("Ingrese la cantidad de discos que desea añadir: ");
+        int cantidadASubir = info.nextInt();
+        for (int i = 0; i < cantidadASubir ; i++) {
+            for (int fila = 0) {
+
+
+        }
+    }
+}
+
